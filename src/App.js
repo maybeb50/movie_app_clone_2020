937bc4be55358c58ccbc726af05ab890d1ed6a5b
maybeb50 <public.maybe.b50@gmail.com>
2020-10-15 18:29:44 +0900

    Movie App Coding...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -36,21 +37,32 @@ class App extends React.Component {
     const { isLoading, movies } = this.state;
     // console.log(movies);
     return(
-      <div>{isLoading ? 'Loading...' 
-        : movies.map((movie) => {
-          console.log(movie);
-          return(
-            <Movie
-              key={movie.id}
-              id={movie.id} 
-              title={movie.title}
-              year={movie.year}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-            />
-          )
-        })
-      }</div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        )
+        : (
+          <div className="movies">
+            {
+              movies.map((movie) => {
+                return(
+                  <Movie
+                    key={movie.id}
+                    id={movie.id} 
+                    title={movie.title}
+                    year={movie.year}
+                    summary={movie.summary}
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres}
+                  />
+                )
+              })
+            }
+          </div>
+        )
+      }</section>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,17 +1,39 @@
 import React, { component } from 'react';
-import Proptype from 'prop-types';
+import PropTypes from 'prop-types';
+import './Movie.css';
 
-function Movie({ id, title, year, summary, poster }) {
-    return <h1>{ title }</h1>;
+function Movie({ title, year, summary, poster, genres }) {
+    return (
+        <div className="movie">
+            <img src={ poster } alt={ title } title={ title } />
+            <div className="movie__data">
+                <h3 className="movie__title">{ title }</h3>
+                <h4 className="movie__year">{ year }</h4>
+                <ul className="movie__genres">
+                    {
+                        genres.map((genre, index) => {
+                            return <li  key={ index } className="movie__genre">{ genre }</li>;
+                        })
+                    }
+                </ul>
+                <p className="movie__summary">
+                    {
+                       summary.slice(0, 180) 
+                    }
+                    ...
+                </p>
+            </div>
+        </div>
+    );
 }
 
-Movie.proptype = {
-    id: Proptype.number.isRequired,
-    year: Proptype.number.isRequired,
-    title: Proptype.string.isRequired,
-    summary: Proptype.string.isRequired,
-    poster: Proptype.string.isRequired,
-    rating: Proptype.number,
+Movie.propTypes = {
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    rating: PropTypes.number,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,     // ???
 };
 
 export default Movie;
