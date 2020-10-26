f59bdfbf6b380f80fcd018a08fa1ea84a7d0fbfb
maybeb50 <public.maybe.b50@gmail.com>
2020-10-26 18:26:45 +0900

    moize library test

--- src/MoizeTest.js
+++ src/MoizeTest.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
 import moize from 'moize'; 
-import Momoize from './Momoize';
+// import Momoize from './Momoize';
 
 class MoizeTest extends Component {
-     render() {
-         const MomoizedFoo = moize.react(Momoize, { isDeepEqual: true });
+    componentDidMount() {
+        const method = (a, b) => a + b;
+        const memoized = moize(method);
 
-         return (
-             <div>
-                 {
-                     'I will memoize the following entry:'
-                 }
-                 <MomoizedFoo />
-             </div>
-         )
+        console.log(memoized(2,4));     // 6
+        console.log(memoized(2,4));     // 6 <- 캐시에서 가져옴 
+        console.log(memoized(5,10));
+    }
+     render() {
+         return <div></div>;
      }
+    //  render() {
+    //      const MomoizedFoo = moize.react(Momoize, { isDeepEqual: true });
+
+    //      return (
+    //          <div>
+    //              {
+    //                  'I will memoize the following entry:'
+    //              }
+    //              <MomoizedFoo />
+    //          </div>
+    //      )
+    //  }
 }
 
 export default MoizeTest;
